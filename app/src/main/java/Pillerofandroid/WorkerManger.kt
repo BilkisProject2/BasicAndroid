29bbdb2bbeec02a89642111557fe0ceafd7c0f7a
BILKIS <androcoding.123@gmail.com>
2025-05-04 22:12:42 +0530

    service and broadcastreceiver

---
+++ app/src/main/java/Pillerofandroid/WorkerManger.kt
@@ -0,0 +1,42 @@
+package Pillerofandroid
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.work.BackoffPolicy
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequest
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
+import com.example.androidpractice.R
+import java.util.concurrent.TimeUnit
+
+class WorkerManger : AppCompatActivity() {
+
+    private val workmanger= WorkManager.getInstance(this)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_worker_manger2)
+    dowork()
+    }
+
+    private fun dowork() {
+        val equets = OneTimeWorkRequest.Builder(workerclass::class.java)
+//        val equets = PeriodicWorkRequest.Builder(workerclass::class.java,15,TimeUnit.MINUTES)
+            .setConstraints(Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build())
+            .setBackoffCriteria(    BackoffPolicy.LINEAR,10,TimeUnit.SECONDS)
+            .build()
+        workmanger.enqueue(equets)
+        workmanger.beginWith(equets).then(equets).enqueue()
+        workmanger.getWorkInfoByIdLiveData(equets.id).observe(this){
+            if (it!=null){
+                printStatus(it.state.name)
+            }
+        }
+    }
+
+    private fun printStatus(name: String) {
+        Log.d("TAG",  name)
+    }
+}
---
+++ app/src/main/java/Pillerofandroid/loggerservice.kt
@@ -0,0 +1,74 @@
+package Pillerofandroid
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import com.example.androidpractice.MainActivity
+import com.example.androidpractice.R
+import kotlin.concurrent.thread
+
+class loggerservice:Service() {
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    //1
+    // this call one time only
+    override fun onCreate() {
+        Log.d("TAG", "onCreate: ")
+        super.onCreate()
+
+    }
+
+    override fun onDestroy() {
+        Log.d("TAG", "onDestroy: ")
+        super.onDestroy()
+    }
+
+    //2 this multile time
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.d("TAG", "onStartCommand: ")
+        thread(start = true){
+            Log.d("TAG", "onStartCommand:2 ")
+            Thread.sleep(1000)
+        }
+        startloggerforgrounservoce()
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    fun startloggerforgrounservoce(){
+        cretaenotificationchannel()
+        val n= createnotification()
+        startForeground(111,n)
+    }
+
+    fun createnotification():Notification{
+        val notification = NotificationCompat.Builder(this,"ID")
+            .setContentText("FORGROUNDSERVICE")
+            .setContentTitle("BILKIS")
+            .setContentIntent(getpentedndingintet())
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setOngoing(true)
+            .build()
+        return  notification
+    }
+
+    fun getpentedndingintet():PendingIntent{
+        val intent = Intent(this,Pillerofandroid.Service::class.java)
+        return PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_IMMUTABLE)
+    }
+
+    fun cretaenotificationchannel():NotificationChannel{
+        val channel = NotificationChannel("ID","BILKIS",NotificationManager.IMPORTANCE_DEFAULT)
+        val notificationMnager= ContextCompat.getSystemService(this,NotificationManager::class.java)
+        notificationMnager!!.createNotificationChannel(channel)
+        return channel
+    }
+}
